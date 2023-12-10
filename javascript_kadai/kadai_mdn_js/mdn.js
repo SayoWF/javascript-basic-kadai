af4d4ecd466e201e84cdfb3f274591742ac38695
Sayo.WF <sayowaki@gmail.com>
2023-12-10 20:57:06 +0900

    mdn.js修正

--- javascript_kadai/kadai_mdn_js/mdn.js
+++ javascript_kadai/kadai_mdn_js/mdn.js
@@ -1,7 +1,7 @@
 //実行する日付を今日で定義する
 const today = new Date();
 const year = today.getFullYear();
-const month = today.getMonth();
+const month = today.getMonth() + 1;
 const date = today.getDate();
 
 //実行する日付を西暦●●●●年●●月●●日と表示する
